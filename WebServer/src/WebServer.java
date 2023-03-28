530f922bf5f130e853543c487c1bee6cc3a4305f
xxking_crashxx <ajdecker2@gmail.com>
2023-03-28 16:51:21 -0400

    added console messages detailing server events.

--- WebServer/src/WebServer.java
+++ WebServer/src/WebServer.java
@@ -8,18 +8,27 @@ public final class WebServer
 	{
 		// Set the port number.
 		int port = 6789;
+		ServerSocket serverSocket = null;
+
 		try {
-			ServerSocket serverSocket = new ServerSocket(port);
+			serverSocket = new ServerSocket(port);
+			System.out.println("http webserver hosted on port: " + port);
 			// Process HTTP service requests in an infinite loop.
 			while (true) {
 				// Listen for a TCP connection request.
+				System.out.println("waiting for client connection...");
 				Socket clientSocket = serverSocket.accept();
+				
+				System.out.println("Client:" + clientSocket.getInetAddress().toString() + " connecting on:" + clientSocket.getPort());
 				new Thread(new HTTPRequestHandler(clientSocket)).start();
 			}
-			//serverSocket.close();
 		}
 		catch(IOException e){
 			e.printStackTrace();
+		}
+		finally{
+			if(serverSocket != null)
+				serverSocket.close();
 		}
 	}
 }
