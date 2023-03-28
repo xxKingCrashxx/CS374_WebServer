8190ca3322d98391ce4212270653c27714c270c3
xxking_crashxx <ajdecker2@gmail.com>
2023-03-27 22:08:00 -0400

    some code additions and refactoring.

--- WebServer/src/HTTPMessageRequest.java
+++ WebServer/src/HTTPMessageRequest.java
@@ -5,15 +5,17 @@ public class HTTPMessageRequest {
 
     private String method;
     private String resource;
+    private String httpVersion;
 
     /**
      * 
      * @param method Method of the http message request. GET, PUT, POST, ETC.
      * @param resource URL / file location of the the requested resource.
      */
-    public HTTPMessageRequest(String method, String resource){
+    public HTTPMessageRequest(String method, String resource, String httpVersion){
         this.method = method;
         this.resource = resource;
+        this.httpVersion = httpVersion;
     }
 
     public String getMethod() {
@@ -22,6 +24,10 @@ public class HTTPMessageRequest {
 
     public String getResource() {
         return resource;
+    }
+
+    public String getHttpVersion() {
+        return httpVersion;
     }
     
 }
--- WebServer/src/HTTPRequestHandler.java
+++ WebServer/src/HTTPRequestHandler.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 public final class HTTPRequestHandler implements Runnable
@@ -15,11 +21,38 @@ public final class HTTPRequestHandler implements Runnable
 	public void run()
 	{
 		//TODO
+		try 
+		{
+			processRequest();
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
 	}
 
 	private void processRequest() throws Exception
 	{
 		//TODO
+		InputStream clientInput = clientSocket.getInputStream();
+		OutputStream clientOutput = clientSocket.getOutputStream();
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientInput));
+
+		String line = bufferedReader.readLine();
+		System.out.println(line);
+
+		String[] parsedLine = line.split(" ");
+		HTTPMessageRequest request = new HTTPMessageRequest(parsedLine[0], parsedLine[1], parsedLine[2]);
+
+		String headerLine;
+		while((headerLine = bufferedReader.readLine()).length() != 0){
+			System.out.println(headerLine);
+		}
+
+		
+
+
+		
 	}
     
 }
